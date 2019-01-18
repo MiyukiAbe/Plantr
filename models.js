939f67b21b36cf4c40ac7e6b25c3e2265f75a64c
Miyuki Abe <miyuki0809@gmail.com>
2019-01-18 13:32:36 -0500

    updated

--- models.js
+++ models.js
@@ -1,10 +1,28 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
 
+const Gardener = db.define('gardener', {
+  name: Sequelize.STRING,
+  age: Sequelize.SMALLINT
+})
 
+const Plot = db.define('plot', {
+  size: Sequelize.SMALLINT,
+  shaded: Sequelize.BOOLEAN
+})
 
+const Vegetable = db.define('vegetable', {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE
+})
 
+Plot.belongsTo(Gardener);
+Gardener.hasMany(Plot);
+Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
 
+//Gardener.hasMany(Vegetable);
 
 
 
